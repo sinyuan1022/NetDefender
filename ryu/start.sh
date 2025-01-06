7df2c28268e6c41a1fab27c9b5635826ee4747fe
sinyuan1022 <113338174+sinyuan1022@users.noreply.github.com>
2025-01-06 15:50:17 +0800

    Update start.sh

--- ryu/start.sh
+++ ryu/start.sh
@@ -1,6 +1,24 @@
+if [ -z "$1" ]; then
+    echo "請提供網路介面名稱，例如: bash ./start.sh ens33"
+    exit 1
+fi
+
+INTERFACE=$1
+
+if ! ip link show "$INTERFACE" &>/dev/null; then
+    echo "網路介面 $INTERFACE 不存在"
+    exit 1
+fi
+
 ovs-vsctl add-br br0
-ifconfig ens33 0
+
+ifconfig $INTERFACE 0
 ifconfig br0 0
-ovs-vsctl add-port br0 ens33
+
+ovs-vsctl add-port br0 $INTERFACE
+
 dhclient br0
+
 ovs-vsctl set-controller br0 tcp:127.0.0.1:6633
+
+echo "設置完成，網路介面 $INTERFACE 已成功配置"
