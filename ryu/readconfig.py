f64eb9ecccbb534c743020a6d86ca265f7dc4a54
sinyuan1022 <113338174+sinyuan1022@users.noreply.github.com>
2026-02-05 16:39:58 +0800

    Update readconfig.py

--- ryu/readconfig.py
+++ ryu/readconfig.py
@@ -5,13 +5,12 @@ def config():
     try:
         with open('config.json', 'r') as f:
             config = json.load(f)
-            
         port_to_container = {}
-        
+        monitor_port = []
+        return_port = []
         for container in config.get('containers', []):
             if 'port' in container and 'image_name' in container:
                 port = container['port']
-                
                 container_info = {
                     'image_name': container['image_name'],
                     'target_port': container.get('target_port', port),
@@ -19,21 +18,24 @@ def config():
                     'command': container.get('command', ''),
                     'multi': container.get('multi', 'no'),
                     'max': int(container.get('max', 1)),
-                    'max_containers':int(container.get('max_containers', 1))
+                    'max_containers':int(container.get('max_containers', 1)),
+                    "send_response": container.get('send_response', 'no')
                 }
-                
                 if port not in port_to_container:
                     port_to_container[port] = []
-                    
+                if container['send_response'] == "yes":
+                    monitor_port.append(port)
+                    return_port.append(container['target_port'])
+
                 port_to_container[port].append(container_info)
-                
-        return port_to_container
+
+        return port_to_container,monitor_port,return_port
     except FileNotFoundError:
         print("Error: Configuration file 'config.json' not found.")
-        return {}
+        return {},[],[]
     except json.JSONDecodeError:
         print("Error: Invalid JSON format in 'config.json'.")
-        return {}
+        return {},[],[]
     except Exception as e:
         print(f"Error reading configuration: {e}")
-        return {}
+        return {},[],[]
